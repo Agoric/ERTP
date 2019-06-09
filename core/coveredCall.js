908e6a45690d02b70dcb18f56961f752c9c3a527
Dan Connolly <dckc@madmode.com>
2019-06-08 22:28:57 -0500

    types for escrow, coveredCall

--- core/coveredCall.js
+++ core/coveredCall.js
@@ -1,9 +1,29 @@
 /* global E */
 // Copyright (C) 2019 Agoric, under Apache License 2.0
+// @flow
 
 import harden from '@agoric/harden';
 
-function coveredCall(terms, inviteMaker) {
+/* ::
+import type { Amount, Payment } from './issuers.chainmail';
+import type { InviteMaker, Timer } from './issuers.chainmail';
+
+import { E } from '@agoric/swingset-vat';
+
+import type { EscrowSeat } from './escrow';
+
+export interface CoveredCallSeat<Money, Stock> {
+  offer(Payment<Stock>): Promise<Payment<mixed>>;
+  getWinnings(): Promise<Payment<Money>>;
+  getRefund(): Promise<Payment<Stock> | null>;
+};
+
+*/
+
+function coveredCall /* :: <Money, Stock> */(
+  terms /* : [any, Amount<Money>, Amount<Stock>, Promise<Timer>, number] */,
+  inviteMaker /* : InviteMaker */,
+) {
   const [
     escrowExchangeInstallationP,
     moneyNeeded,
@@ -12,16 +32,24 @@ function coveredCall(terms, inviteMaker) {
     deadline,
   ] = terms;
 
-  const pairP = E(escrowExchangeInstallationP).spawn(
-    harden([moneyNeeded, stockNeeded]),
-  );
+  /* ::
+  type PairInv = [Payment<any>, Payment<any>];
+  */
 
-  const aliceEscrowSeatP = E.resolve(pairP).then(pair =>
-    inviteMaker.redeem(pair[0]),
-  );
-  const bobEscrowSeatP = E.resolve(pairP).then(pair =>
-    inviteMaker.redeem(pair[1]),
-  );
+  // eslint-disable-next-line prettier/prettier
+  const pairP = E /* :: <any, PairInv> */(
+    escrowExchangeInstallationP,
+  ).spawn(harden([moneyNeeded, stockNeeded]));
+
+  // ISSUE: type of redeem() is by inspection of contract source; we use any.
+  // eslint-disable-next-line prettier/prettier
+  const aliceEscrowSeatP /* : Promise<EscrowSeat<Money, Stock>> */ = E.resolve /* :: <PairInv> */(
+    pairP,
+  ).then(([am, _as]) => inviteMaker.redeem(am));
+  // eslint-disable-next-line prettier/prettier
+  const bobEscrowSeatP /* : Promise<EscrowSeat<Stock, Money>> */ = E.resolve /* :: <PairInv> */(
+    pairP,
+  ).then(([_am, as]) => inviteMaker.redeem(as));
 
   // Seats
 
@@ -29,11 +57,15 @@ function coveredCall(terms, inviteMaker) {
     .delayUntil(deadline)
     .then(_ => E(bobEscrowSeatP).cancel('expired'));
 
-  const bobSeat = harden({
-    offer(stockPayment) {
+  function ep /* :: <T> */(x /* : T */) /* : Promise<T> */ {
+    return Promise.resolve(x);
+  }
+
+  const bobSeat /* : CoveredCallSeat<Money, Stock> */ = harden({
+    offer(stockPayment /* : Payment<Stock> */) {
       const sIssuer = stockNeeded.label.issuer;
-      return E(sIssuer)
-        .getExclusive(stockNeeded, stockPayment, 'prePay')
+      return E(ep(sIssuer))
+        .getExclusive(stockNeeded, ep(stockPayment), 'prePay')
         .then(prePayment => {
           E(bobEscrowSeatP).offer(prePayment);
           return inviteMaker.make('holder', aliceEscrowSeatP);
@@ -50,6 +82,7 @@ function coveredCall(terms, inviteMaker) {
   return inviteMaker.make('writer', bobSeat);
 }
 
+// $FlowFixMe flow thinks "function [1] should not be coerced"
 const coveredCallSrc = `(${coveredCall})`;
 
 export { coveredCall, coveredCallSrc };
--- core/escrow.js
+++ core/escrow.js
@@ -1,7 +1,33 @@
 /* global E makePromise */
 // Copyright (C) 2019 Agoric, under Apache License 2.0
+// @flow
 
 import harden from '@agoric/harden';
+
+/* ::
+import { E } from '@agoric/swingset-vat';
+
+import makePromise from '../util/makePromise';
+import type { PromiseParts } from '../util/makePromise';
+import type { G, Amount, Assay, Label, Payment } from './issuers.chainmail';
+import type { InviteMaker } from './issuers.chainmail';
+
+export interface EscrowSeat<Money, Stock> {
+  offer(Payment<Money>): void;
+  cancel(reason: mixed): void;
+  getWinnings(): Promise<Payment<Stock>>;
+  getRefund(): Promise<Payment<Money> | null>;
+};
+
+interface Transfer<Q> {
+  phase1(): Promise<Payment<Q>>;
+  phase2(): void;
+  abort(reason: mixed): void;
+  getWinnings(): Promise<Payment<Q>>;
+  getRefund(): Promise<Payment<Q> | null>;
+}
+
+*/
 
 // For clarity, the code below internally speaks of a scenario is
 // which Alice is trading some of her money for some of Bob's
@@ -10,14 +36,24 @@ import harden from '@agoric/harden';
 // players 0 and 1. Money are the rights transfered from player 0 to
 // 1, and Stock are the rights transfered from 1 to 0.
 
-function escrowExchange(terms, inviteMaker) {
+function escrowExchange /* :: <Money, Stock> */(
+  terms /* : [Amount<Money>, Amount<Stock>] */,
+  inviteMaker /* : InviteMaker */,
+) {
   const [moneyNeeded, stockNeeded] = terms;
 
-  function makeTransfer(amount, srcPaymentP) {
+  function makeTransfer /* :: <Q> */(
+    amount /* : Amount<Q> */,
+    srcPaymentP /* : Promise<Payment<Q>> */,
+  ) /* : Transfer<Q> */ {
     const { issuer } = amount.label;
-    const escrowP = E(issuer).getExclusive(amount, srcPaymentP, 'escrow');
+    const escrowP = E(E.resolve(issuer)).getExclusive(
+      amount,
+      srcPaymentP,
+      'escrow',
+    );
     const winnings = makePromise();
-    const refund = makePromise();
+    const refund /* : PromiseParts<Payment<Q> | null> */ = makePromise();
     return harden({
       phase1() {
         return escrowP;
@@ -42,10 +78,16 @@ function escrowExchange(terms, inviteMaker) {
   // Promise wiring
 
   const moneyPayment = makePromise();
-  const moneyTransfer = makeTransfer(moneyNeeded, moneyPayment.p);
+  const moneyTransfer /* : Transfer<Money> */ = makeTransfer(
+    moneyNeeded,
+    moneyPayment.p,
+  );
 
   const stockPayment = makePromise();
-  const stockTransfer = makeTransfer(stockNeeded, stockPayment.p);
+  const stockTransfer /* : Transfer<Stock> */ = makeTransfer(
+    stockNeeded,
+    stockPayment.p,
+  );
 
   // TODO Use cancellation tokens instead.
   const aliceCancel = makePromise();
@@ -63,7 +105,7 @@ function escrowExchange(terms, inviteMaker) {
       moneyTransfer.phase2();
       stockTransfer.phase2();
     },
-    reason => {
+    (reason /* : mixed */) => {
       moneyTransfer.abort(reason);
       stockTransfer.abort(reason);
     },
@@ -71,14 +113,14 @@ function escrowExchange(terms, inviteMaker) {
 
   // Seats
 
-  const aliceSeat = harden({
+  const aliceSeat /* : EscrowSeat<Money, Stock> */ = harden({
     offer: moneyPayment.res,
     cancel: aliceCancel.reject,
     getWinnings: stockTransfer.getWinnings,
     getRefund: moneyTransfer.getRefund,
   });
 
-  const bobSeat = harden({
+  const bobSeat /* : EscrowSeat<Stock, Money> */ = harden({
     offer: stockPayment.res,
     cancel: bobCancel.reject,
     getWinnings: moneyTransfer.getWinnings,
@@ -91,6 +133,7 @@ function escrowExchange(terms, inviteMaker) {
   ]);
 }
 
+// $FlowFixMe flow thinks "function [1] should not be coerced"
 const escrowExchangeSrc = `(${escrowExchange})`;
 
 export { escrowExchange, escrowExchangeSrc };
