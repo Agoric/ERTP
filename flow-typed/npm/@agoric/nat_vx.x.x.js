1a6b4cc6fcc7d93d1a4770ab0a3c9e5802dea2d5
Dan Connolly <dckc@madmode.com>
2019-06-08 22:26:05 -0500

    types for nat

---
+++ flow-typed/npm/@agoric/nat_vx.x.x.js
@@ -0,0 +1,13 @@
+// flow-typed signature: 3ce82af92ab28726b3f68e98b11010fc
+// flow-typed version: <<STUB>>/@agoric/nat_v2.0.1/flow_v0.100.0
+
+/**
+ * TODO: send a pull request to:
+ * https://github.com/flowtype/flow-typed
+ */
+
+declare module '@agoric/nat' {
+  declare function Nat(allegedNum: mixed): number;
+
+  declare module.exports: typeof Nat;
+}
