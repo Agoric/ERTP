03fdd509cb4a6605e0edeb68b023aef27c634bda
Chris Hibbert <hibbert@agoric.com>
2019-08-05 10:30:42 -0700

    Rename the root test case and add a comment explaining its purpose.
    fix the test after directory move.

--- test/swingsetTests/presenceCorruption/test-corruption.js
+++ test/swingsetTests/presenceCorruption/test-corruption.js
@@ -1,8 +1,14 @@
 import { test } from 'tape-promise/tape';
+import path from 'path';
 import { buildVatController, loadBasedir } from '@agoric/swingset-vat';
 
+// This is a test that demonstrates an issue with the current implementation
+// of eventual send. It passes with the current (2019/08/05) known good state,
+// but eventual send will need some work so it doesn't break multi-vat sharing.
+
 async function main(withSES, basedir, argv) {
-  const config = await loadBasedir(basedir);
+  const dir = path.resolve('test/swingsetTests', basedir);
+  const config = await loadBasedir(dir);
   const ldSrcPath = require.resolve(
     '@agoric/swingset-vat/src/devices/loopbox-src',
   );
@@ -19,9 +25,7 @@ const corruptedPresenceGolden = [
 ];
 
 test('`run corrupted presence with SES', async t => {
-  const dump = await main(true, 'test/presenceCorruption', [
-    'corrupted-presence',
-  ]);
+  const dump = await main(true, 'presenceCorruption', ['corrupted-presence']);
   t.deepEquals(dump.log, corruptedPresenceGolden);
   t.end();
 });
