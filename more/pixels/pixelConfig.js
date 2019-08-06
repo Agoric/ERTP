763e8a1b202434b9b3c2aa5e2cc09005484cca0e
Kate Sills <kate@agoric.com>
2019-08-06 14:47:26 -0700

    add more documentation

--- more/pixels/pixelConfig.js
+++ more/pixels/pixelConfig.js
@@ -3,13 +3,35 @@ import harden from '@agoric/harden';
 import { makePixelMintKeeper } from './pixelMintKeeper';
 import { makePixelListAssayMaker } from './pixelAssays';
 
+/**
+ * `makePixelConfigMaker` exists in order to pass in more parameters
+// than makePixelConfig allows.
+ * @param  {function} makeUseObj creates a "use object", which has all
+ * of the non-ERTP methods for assets that are designed to be used.
+ * For instance, a pixel can be colored. The use object is associated
+ * with an underlying asset that provides the authority to use it. 
+ * @param  {number} canvasSize=10 the size of the gallery in pixel
+ * squares across and down
+ * @param  {issuer} parentIssuer the `parentIssuer` is used when
+ * creating a revocable childPayment or childPurse, as in the
+ * landowner/tenant/subtenant pattern. In that pattern, the owner
+ * holds assets associated with the parent issuer, the tenant holds
+ * assets associated with the child issuer, the subtenant holds assets
+ * associated with the grandchild issuer, and so forth.
+ */
 function makePixelConfigMaker(makeUseObj, canvasSize = 10, parentIssuer) {
   function makePixelConfig(makeMint, description) {
+    // The childIssuer/childMint are lazily created to avoid going
+    // infinitely far down the chain of issuers on creation. The
+    // childIssuer assets are revocable by the current issuer (the
+    // child's parent).
     let childIssuer;
     let childMint;
 
     const makePixelAssay = makePixelListAssayMaker(canvasSize);
 
+    // Lazily creates the childMint if it doesn't already
+    // exist
     function getOrMakeChildMint(issuer) {
       if (childMint === undefined) {
         const makePixelConfigChild = makePixelConfigMaker(
@@ -23,6 +45,9 @@ function makePixelConfigMaker(makeUseObj, canvasSize = 10, parentIssuer) {
       return childMint;
     }
 
+    // This method is used in the creation of childPayments and
+    // childPurses where we want the amount to be the same as in the
+    // original asset apart from the difference in issuers.
     function getChildAmount(issuer, amount) {
       getOrMakeChildMint(issuer);
       // quantity is the same, but amounts are different for
@@ -35,12 +60,15 @@ function makePixelConfigMaker(makeUseObj, canvasSize = 10, parentIssuer) {
       makeCustomPayment(superPayment, issuer) {
         return harden({
           ...superPayment,
+          // This creates a new use object on every call. Please see
+          // the gallery for the definition of the use object that is
+          // created here by calling `makeUseObj`
           getUse() {
             return makeUseObj(issuer, superPayment);
           },
+          // Mint a new payment from the child mint with the same
+          // quantity as the original payment
           getChildPayment() {
-            // quantity is the same, but amounts are different for
-            // different issuers
             const childAmount = getChildAmount(
               issuer,
               superPayment.getBalance(),
@@ -48,42 +76,66 @@ function makePixelConfigMaker(makeUseObj, canvasSize = 10, parentIssuer) {
             const childPurse = childMint.mint(childAmount);
             return childPurse.withdrawAll();
           },
+          // Remove the amount of this payment from the purses and
+          // payments of the childMint. Removes recursively down the
+          // chain until it fails to find a childMint.
           revokeChildren() {
-            const childAmount = getChildAmount(
-              issuer,
-              superPayment.getBalance(),
-            );
-            childMint.revoke(childAmount);
+            if (childMint !== undefined) {
+              const childAmount = getChildAmount(
+                issuer,
+                superPayment.getBalance(),
+              );
+              childMint.revoke(childAmount);
+            }
           },
         });
       },
       makeCustomPurse(superPurse, issuer) {
         return harden({
           ...superPurse,
+          // This creates a new use object on every call. Please see
+          // the gallery for the definition of the use object that is
+          // created here by calling `makeUseObj`
           getUse() {
             return makeUseObj(issuer, superPurse);
           },
+          // Mint a new purse from the child mint with the same
+          // quantity as the original purse
           getChildPurse() {
             const childAmount = getChildAmount(issuer, superPurse.getBalance());
             return childMint.mint(childAmount);
           },
+          // Remove the amount of this purse from the purses and
+          // payments of the childMint. Removes recursively down the
+          // chain until it fails to find a childMint.
           revokeChildren() {
-            const childAmount = getChildAmount(issuer, superPurse.getBalance());
-            childMint.revoke(childAmount);
+            if (childMint !== undefined) {
+              const childAmount = getChildAmount(
+                issuer,
+                superPurse.getBalance(),
+              );
+              childMint.revoke(childAmount);
+            }
           },
         });
       },
       makeCustomMint(superMint, issuer, assay, mintKeeper) {
         return harden({
           ...superMint,
+          // revoke destroys the amount from this mint and calls
+          // revoke on the childMint with an amount of the same
+          // quantity. Destroying the amount depends on the fact that
+          // pixels are uniquely identifiable by their `x` and `y`
+          // coordinates. Therefore, destroy can look for purses and
+          // payments that include those particular pixels and remove
+          // the particular pixels from those purses or payments
           revoke(amount) {
             amount = assay.coerce(amount);
-            // for non-fungible tokens that are unique, revoke them by removing them from
-            // the purses/payments that they live in
 
-            // the amount may not exist in the case of childMint amounts, so
-            // catch the error
-
+            // wrap this in a try/catch because sometimes the amount
+            // we are trying to destroy will not have been made yet,
+            // in the case of a childMint.
+            // TODO: handle this better
             try {
               mintKeeper.destroy(amount);
               if (childMint !== undefined) {
@@ -98,13 +150,18 @@ function makePixelConfigMaker(makeUseObj, canvasSize = 10, parentIssuer) {
       makeCustomIssuer(superIssuer) {
         return harden({
           ...superIssuer,
+          // The parent issuer is one level up in the chain of
+          // issuers.
           getParentIssuer() {
             return parentIssuer;
           },
+          // The child issuer is one level down in the chain of issuers.
           getChildIssuer() {
             getOrMakeChildMint(superIssuer);
             return childIssuer;
           },
+          // Returns true if the alleged descendant issuer is either a
+          // child, grandchild, or any other kind of descendant
           isDescendantIssuer(allegedDescendant) {
             if (childIssuer === undefined) {
               return false;
