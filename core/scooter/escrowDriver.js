5d2260b9c52aaf798aa6c6841c51bdc031dca368
Mark S. Miller <erights@gmail.com>
2019-08-12 08:34:50 -0700

    escrow diver checks offer safety

--- core/scooter/escrowDriver.js
+++ core/scooter/escrowDriver.js
@@ -1,59 +1,86 @@
 /* global E */
 
 import harden from '@agoric/harden';
-import {
-  isOfferSafe,
-  areOffersSafe,
-  offerTotals,
-  areAmountsConserved,
-} from './scooterUtils';
 
+import { insist } from '../../util/insist';
+import { makePeg } from '../issuers';
+import { areOffersSafe } from './scooterUtils';
 
-const escrowExchange = harden({
-  start: (terms, inviteMaker) => {
+const escrowDriver = harden({
+  start: (terms, _inviteMaker) => {
     const {
-      neededAmounts: [...neededAmounts],
       scooterInstallation: scooterInstallationP,
+      issuers: [...issuerPs],
     } = terms;
 
-    const issuerPs = neededAmounts.map(amount => amount.label.issuer);
+    insist(issuerPs.length === 2)`\
+Must be exactly two issuers: ${terms.issuers}`;
+
     const scooterP = E(scooterInstallationP).spawn(harden({ issuerPs }));
 
-    const offerIds = [];
-    const descriptions = [];
-    let statuses = [];
-    let isShutdown = false;
+    // Currently, we omit the optional args to makePeg, so currently
+    // scooter is only compatible with simple default Nat
+    // issuers. However, we try to keep much of the code below
+    // general.
+    const pegPs = issuerPs.map(issuerP => makePeg(E, issuerP));
+    return Promise.all(pegPs).then(pegs => {
+      const assays = pegs.map(peg => peg.getLocalIssuer().getAssay());
 
-    function shutdown() {
-      statuses = statuses.map(status => ({ ...status, isInPool: false }));
-      E(scooterP).updateOffers(offerIds, statuses);
-      offerIds.length = descriptions.length = statuses.length = 0;
-      isShutdown = true;
-    }
+      // TODO Do these mutable arrays pass Jessie safety rules?
+      const offerIds = [];
+      const descriptions = [];
+      let statuses = [];
+      let isShutdown = false;
 
-    function makeInvite(offeredSide, neededSide) {
-      const sentry = harden({
-        noticeOfferEntered(offerId, description, status) {
-          offerIds.push(offerId);
-          descriptions.push(description);
-          statuses.push(status);
+      function shutdown(lastState) {
+        statuses = statuses.map(status => ({
+          ...status,
+          offerState: lastState,
+          isInPool: false,
+        }));
+        E(scooterP).updateOffers(offerIds, statuses);
 
-          if (isShutdown) {
-            shutdown();
-          } else if (offerIds.length === 2) {
-            const newStatuses = [
-              { ...statuses[0], balances: statuses[1].balances },
-              { ...statuses[1], balances: statuses[0].balances },
-            ];
-            shutdown();
-          }
-        },
-        noticeOfferExitRequested(offerId) {
-          shutdown();
-        },
-      });
-      return E(scooterP).inviteToPlaceOffer(sentry, offeredSide, neededSide);
-    }
-    return harden([makeInvite(0, 1), makeInvite(1, 0)]);
+        offerIds.length = 0;
+        descriptions.length = 0;
+        statuses.length = 0;
+        isShutdown = true;
+      }
+
+      function makeInvite(offeredSide, neededSide) {
+        const sentry = harden({
+          noticeOfferEntered(offerId, description, status) {
+            offerIds.push(offerId);
+            descriptions.push(description);
+            statuses.push(status);
+
+            if (isShutdown) {
+              shutdown('already shut down');
+            } else if (offerIds.length === 2) {
+              const newStatuses = [
+                { ...statuses[0], balances: statuses[1].balances },
+                { ...statuses[1], balances: statuses[0].balances },
+              ];
+              if (areOffersSafe(assays, descriptions, newStatuses)) {
+                statuses = newStatuses;
+                shutdown('traded');
+              } else {
+                shutdown('trade aborted');
+              }
+            }
+          },
+          noticeOfferExitRequested(_offerId) {
+            shutdown('trade cancelled');
+          },
+        });
+        return E(scooterP).inviteToPlaceOffer(sentry, offeredSide, neededSide);
+      }
+      return harden([makeInvite(0, 1), makeInvite(1, 0)]);
+    });
   },
 });
+
+const escrowDriverSrcs = harden({
+  start: `${escrowDriver.start}`,
+});
+
+export { escrowDriverSrcs };
