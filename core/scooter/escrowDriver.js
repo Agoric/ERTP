6104e74ac4fd617f221fdf2f42a853df8c016645
Mark S. Miller <erights@gmail.com>
2019-08-12 08:49:59 -0700

    commentary

--- core/scooter/escrowDriver.js
+++ core/scooter/escrowDriver.js
@@ -18,6 +18,12 @@ Must be exactly two issuers: ${terms.issuers}`;
 
     const scooterP = E(scooterInstallationP).spawn(harden({ issuerPs }));
 
+    // TODO The peg-to-assay logic here is borrowed from
+    // scooter.js. Can we factor some of the common logic into
+    // scooterUtils.js? Here, we only need the local assays with the
+    // remote labels. Can we easily just get them without making pegs,
+    // issuers, and all the rest?
+    //
     // Currently, we omit the optional args to makePeg, so currently
     // scooter is only compatible with simple default Nat
     // issuers. However, we try to keep much of the code below
@@ -26,10 +32,12 @@ Must be exactly two issuers: ${terms.issuers}`;
     return Promise.all(pegPs).then(pegs => {
       const assays = pegs.map(peg => peg.getLocalIssuer().getAssay());
 
-      // TODO Do these mutable arrays pass Jessie safety rules?
-      const offerIds = [];
-      const descriptions = [];
-      let statuses = [];
+      // TODO The scooter API pushes us into this columnar
+      // representation. Should we instead have a row-oriented
+      // representation? Should the scotter API be more row oriented?
+      let offerIds = harden([]);
+      let descriptions = harden([]);
+      let statuses = harden([]);
       let isShutdown = false;
 
       function shutdown(lastState) {
@@ -40,18 +48,23 @@ Must be exactly two issuers: ${terms.issuers}`;
         }));
         E(scooterP).updateOffers(offerIds, statuses);
 
-        offerIds.length = 0;
-        descriptions.length = 0;
-        statuses.length = 0;
+        offerIds = harden([]);
+        descriptions = harden([]);
+        statuses = harden([]);
         isShutdown = true;
       }
 
       function makeInvite(offeredSide, neededSide) {
         const sentry = harden({
           noticeOfferEntered(offerId, description, status) {
-            offerIds.push(offerId);
-            descriptions.push(description);
-            statuses.push(status);
+            // TODO Could Jessie safety rules allow us to push onto
+            // mutable arrays?
+            // TODO This "pushes" in arrival order. Should we instead
+            // have fixed positions or something for fixed seats? It
+            // would better serve the other contracts.
+            offerIds = harden([...offerIds, offerId]);
+            descriptions = harden([...descriptions, description]);
+            statuses = harden([...statuses, status]);
 
             if (isShutdown) {
               shutdown('already shut down');
