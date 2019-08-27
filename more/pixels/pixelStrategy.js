b82dfcda0602eb726a745204d780ec20d338b8cc
Kate Sills <kate@agoric.com>
2019-08-26 19:25:31 -0700

    move the pixel list logic into the strategy

--- more/pixels/pixelStrategy.js
+++ more/pixels/pixelStrategy.js
@@ -1,34 +1,74 @@
 import harden from '@agoric/harden';
 
-import {
-  insistPixelList,
-  includesPixelList,
-  withPixelList,
-  withoutPixelList,
-} from './types/pixelList';
+import { passStyleOf } from '@agoric/marshal';
+
+import { insistPixel, isEqual } from './types/pixel';
+import { insist } from '../../util/insist';
+
+function includesPixel(pixelList, pixel) {
+  let result = false;
+  for (const p of pixelList) {
+    if (isEqual(pixel, p)) {
+      result = true;
+    }
+  }
+  return result;
+}
 
 // A pixelList is a naive collection of pixels in the form:
 // [ { x: 0, y: 0 }, { x: 1, y: 1} ...]
 // This is less than ideal for efficiency and expressiveness but will
 // do for now
-const makePixelStrategy = canvasSize => {
+const makePixelStrategy = (canvasSize = 10) => {
   const pixelStrategy = harden({
     insistKind: pixelList => {
-      insistPixelList(pixelList, canvasSize);
+      insist(
+        passStyleOf(harden(pixelList)) === 'copyArray',
+      )`pixelList must be an array`;
+      for (const pixel of pixelList) {
+        insistPixel(pixel, canvasSize);
+      }
       return harden(pixelList);
     },
     empty: _ => harden([]),
     isEmpty: pixelList => pixelList.length === 0,
-    includes: (whole, part) => includesPixelList(whole, part),
+    includes: (whole, part) => {
+      for (const partPixel of part) {
+        const result = includesPixel(whole, partPixel);
+        if (!result) {
+          return false; // return early if false
+        }
+      }
+      return true;
+    },
     equals: (left, right) =>
       pixelStrategy.includes(left, right) &&
       pixelStrategy.includes(right, left),
-    with: (left, right) => harden(withPixelList(left, right)),
-    without: (whole, part) => harden(withoutPixelList(whole, part)),
+    with: (left, right) => {
+      const combinedList = Array.from(left);
+      for (const rightPixel of right) {
+        if (!includesPixel(left, rightPixel)) {
+          combinedList.push(rightPixel);
+        }
+      }
+      return combinedList;
+    },
+    without: (whole, part) => {
+      insist(
+        pixelStrategy.includes(whole, part),
+      )`leftPixelList is not in rightPixelList`;
+      const wholeMinusPart = [];
+      for (const wholePixel of whole) {
+        if (!includesPixel(part, wholePixel)) {
+          wholeMinusPart.push(wholePixel);
+        }
+      }
+      return wholeMinusPart;
+    },
   });
   return pixelStrategy;
 };
 
 harden(makePixelStrategy);
 
-export { makePixelStrategy };
+export { makePixelStrategy, includesPixel };
