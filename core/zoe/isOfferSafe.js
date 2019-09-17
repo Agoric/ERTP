f6526e89384d0c08d66869401e5df700aff59fd0
Kate Sills <kate@agoric.com>
2019-09-16 18:02:53 -0700

    quick fixes from review

--- core/zoe/isOfferSafe.js
+++ core/zoe/isOfferSafe.js
@@ -2,22 +2,23 @@ import { allTrue } from './utils';
 import { insist } from '../../util/insist';
 
 /**
- * @param  {array} assaysPerIssuer - an array of assays ordered in the
- * same order as the corresponding issuers
- * @param  {array} rulesPerIssuer - an array of "rules" ordered in the
- * same order as the corresponding issuers. Rules are a player's
- * understanding of the contract that they are entering when they make
- * an offer. Rules are structured in the form { rule: '', amount}
- * @param  {array} amountsPerIssuer - an array of amounts ordered in
- * the same order as the corresponding issuers. This array of amounts
- * is the reallocation to be given to a player.
- *
  * `isOfferSafeForPlayer` checks offer-safety for a single player.
  *
  * Note: This implementation checks whether we refund for all rules or
  * return winnings for all rules. It does not allow some refunds and
  * some winnings, which is what would happen if you checked the rules
  * independently.
+ *
+ * @param  {array} assaysPerIssuer - an array of assays ordered in the
+ * same order as the corresponding issuers
+ * @param  {array} rulesPerIssuer - an array of "rules" ordered in the
+ * same order as the corresponding issuers. Rules are a player's
+ * understanding of the contract that they are entering when they make
+ * an offer. Rules are structured in the form `{ rule:
+ * descriptionString, amount}`
+ * @param  {array} amountsPerIssuer - an array of amounts ordered in
+ * the same order as the corresponding issuers. This array of amounts
+ * is the reallocation to be given to a player.
  */
 function isOfferSafeForPlayer(
   assaysPerIssuer,
@@ -46,7 +47,7 @@ function isOfferSafeForPlayer(
       }
       return true;
     })
-    .reduce(allTrue, true);
+    .reduce(allTrue);
 
   // If we are not refunding the player, are their allocated amounts
   // greater than or equal to what they said they wanted at the beginning?
@@ -60,7 +61,7 @@ function isOfferSafeForPlayer(
       }
       return true;
     })
-    .reduce(allTrue, true);
+    .reduce(allTrue);
 
   return refundOk || winningsOk;
 }
@@ -69,10 +70,10 @@ function isOfferSafeForPlayer(
  * their corresponding issuers
  * @param  {matrix} offerMatrix - an array of arrays. Each of the
  * element arrays is the offer (array of rules) that a single player
- * made. In the same order as the corresponding issuers.
+ * made, in the same order as the corresponding issuers.
  * @param  {matrix} amountMatrix - an array of arrays. Each of the
  * element arrays is the array of amounts that a single player will
- * get. In the same order as the corresponding issuers.
+ * get, in the same order as the corresponding issuers.
  */
 function isOfferSafeForAll(assays, offerMatrix, amountMatrix) {
   return offerMatrix
